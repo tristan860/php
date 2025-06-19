2d7e5fa166674c5eec057fd351f91f2cbd3326c9
Tristan Jugapuu <52077671+tristan860@users.noreply.github.com>
2025-06-19 17:07:22 +0300

    dsadsadsadasdsa

--- iseseisev/index.php
+++ iseseisev/index.php
@@ -1,39 +1,35 @@
-<?php
-$page = $_GET['page'] ?? 'avaleht';
-$lubatud = ['avaleht', 'tooted', 'admin', 'kontakt'];
-?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Index</title>
+    <title>iseseisev</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 <body>
     <div class="container">
+<?php
+// iseseisev
+// Tristan Juapuu
+// 19.06.2025
+?>
+<div class="container bg-light">  
 
-<span class="fw-bold fs-5">Tristan.ee</span>
-        <nav class="nav">
-            <a class="nav-link <?php if($page=='avaleht') echo 'active'; ?>" href="?page=avaleht">Avaleht</a>
-            <a class="nav-link <?php if($page=='kontakt') echo 'active'; ?>" href="?page=kontakt">Kontakt</a>
-            <a class="nav-link <?php if($page=='admin') echo 'active'; ?>" href="?page=admin">Admin</a>
-        </nav>
-    </div>
-</header>
-<div class="container py-4">
-    <?php
-    if (!in_array($page, $lubatud)) {
-        echo '<div class="alert alert-danger">Lehte ei eksisteeri!</div>';
-    } else {
-        include $page.'.php';
-    }
-    ?>
+        <div class="col-md-3 mb-2 mb-md-0"> 
+                
+            <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none"></a> 
+            <a>Tristan.ee</a>
+        </div> 
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-end mb-md-0"> 
+            
+            <li>
+                <a href="avaleht.php" class="nav-link px-2">Avaleht</a>
+            </li> 
+            <li>
+                <a href="kontakt.php" class="nav-link px-2">Kontakt</a>
+            </li> 
+            <li>
+                <a href="admin.php" class="nav-link px-2">Admin</a>
+            </li> 
+        </ul> 
 </div>
-<footer class="bg-light text-center py-4 mt-5 border-top">
-    <div class="container">
-        <small>Tristan</small>
-    </div>
-</footer>
-</body>
-</html>
--- iseseisev/kontakt.php
+++
@@ -1,2 +0,0 @@
-<h2>Kontakt</h2>
-<p>Võta meiega ühendust: tristanjugapuu@gmail.com</p>
